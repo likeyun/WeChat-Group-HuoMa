77c1f4dfed23a2643b08334cb735190a81e74021
TANKING <king@likeyunba.com>
2020-07-16 11:19:54 +0800

    Add files via upload

---
+++ admin/upload.php
@@ -0,0 +1,54 @@
+<?php
+header("Content-type:application/json");
+ 
+//获取原始文件名
+$filename = $_FILES["file"]["name"];
+ 
+//获取文件后缀名
+$hzm = substr($filename,strpos($filename,"."));
+ 
+//设置新文件名
+$newfilename = date("Y-m-d")."-".rand(10000,99999);
+ 
+// 允许上传的图片后缀
+$allowedExts = array("gif", "jpeg", "jpg", "png");
+$temp = explode(".", $filename);
+$extension = end($temp);
+if ((($_FILES["file"]["type"] == "image/gif")
+|| ($_FILES["file"]["type"] == "image/jpeg")
+|| ($_FILES["file"]["type"] == "image/jpg")
+|| ($_FILES["file"]["type"] == "image/pjpeg")
+|| ($_FILES["file"]["type"] == "image/x-png")
+|| ($_FILES["file"]["type"] == "image/png"))
+&& ($_FILES["file"]["size"] < 2048000)   // 小于 2000 kb
+&& in_array($extension, $allowedExts))
+{
+    if ($_FILES["file"]["error"] > 0)
+    {
+        echo "{\"res\":\"404\"}";
+    }
+    else
+    {
+    // 此处可以输出文件的详细信息
+    if (file_exists("upload/" . $newfilename.$hzm))
+        {
+            //
+        }
+        else
+        {
+            move_uploaded_file($_FILES["file"]["tmp_name"], "upload/" . $newfilename.$hzm);
+            $current_url = 'http://'.$_SERVER['SERVER_NAME'].$_SERVER["REQUEST_URI"];
+            $current_wenjianjia = dirname($current_url);
+            $upload_result = array(
+                "path" => $current_wenjianjia."/upload/".$newfilename.$hzm,
+                "res" => 400
+            );
+            echo json_encode($upload_result,true);
+        }
+    }
+}
+else
+{
+    echo "{\"res\":\"403\"}";
+}
+?>
