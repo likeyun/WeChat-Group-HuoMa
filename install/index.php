caa8065159f0ebb4ca12e42d1d4dc835851ddd4b
TANKING <king@likeyunba.com>
2020-07-16 11:16:36 +0800

    系统安装程序

---
+++ install/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>里客云群活码生成系统安装</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+	<style type="text/css">
+		.container{
+			width: 600px;
+		}
+	</style>
+</head>
+<body>
+<div class="container mt-3">
+  <br/>
+  <br/>
+  <br/>
+  <h3>里客云群活码生成系统安装</h3>
+  <p>请在php5.5 MySql5.0 以上版本安装</p>
+  
+  <form action="db_creat.php" method="post">
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text">数据库地址</span>
+      </div>
+      <input type="text" class="form-control" placeholder="请粘贴数据库服务器地址" name="dbservername">
+    </div>
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text">数据库账号</span>
+      </div>
+      <input type="text" class="form-control" placeholder="请输入数据库账号" name="dbusername">
+    </div>
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text">数据密码</span>
+      </div>
+      <input type="password" class="form-control" placeholder="请输入数据库密码" name="dbpassword">
+    </div>
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text">数据名</span>
+      </div>
+      <input type="text" class="form-control" placeholder="请输入数据库名" name="dbname">
+    </div>
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text">管理员账号</span>
+      </div>
+      <input type="text" class="form-control" placeholder="请设置管理员账号" name="adminuser">
+    </div>
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text">管理员密码</span>
+      </div>
+      <input type="password" class="form-control" placeholder="请设置管理员密码" name="adminpwd">
+    </div>
+    <input type="submit" value="立即安装" class="btn btn-secondary">
+  </form>
+</div>
+</body>
+</html>
