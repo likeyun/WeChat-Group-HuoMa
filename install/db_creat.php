52a6a0097edc00e3327c6aeb5e146210a5bfc844
TANKING <king@likeyunba.com>
2020-09-14 20:55:07 +0800

    修复添加邀请码失败的bug

--- install/db_creat.php
+++ install/db_creat.php
@@ -75,12 +75,13 @@ user_quanxian VARCHAR(32) NOT NULL,
 user_email VARCHAR(32) NOT NULL
 )";
 
-// 创建qun_huoma_user数据表
+// 创建qun_huoma_yqm数据表
 $sql_qun_huoma_yqm = "CREATE TABLE qun_huoma_yqm (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
 yqm VARCHAR(32) NOT NULL,
 yqm_status VARCHAR(32) NOT NULL,
-use_time VARCHAR(32) NOT NULL
+use_time VARCHAR(32) NOT NULL,
+yqm_daynum VARCHAR(32) NOT NULL
 )";
 
 
